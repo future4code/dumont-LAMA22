4eb483251f53e532a93ba9bb5df603403e1b72ba
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2021-02-20 21:48:56 -0300

    User's login finhished

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { UserInputDTO } from "../business/entities/User"
+import { LoginInputDTO, UserInputDTO } from "../business/entities/User"
 import { Authenticator } from "../business/services/Authenticator"
 import { HashManager } from "../business/services/HashManager"
 import { IdGenerator } from "../business/services/IdGenerator"
@@ -33,6 +33,28 @@ export class UserController {
         }
         catch(error){
             res.status(error.statusCode || 400).send({
+                error:error.message
+            })
+        }
+    }
+
+    public login = async(
+        req: Request,
+        res: Response
+    ):Promise<void> =>{
+        try{
+            const loginData: LoginInputDTO = {
+                email: req.body.email,
+                password: req.body.password
+            }
+            const token = await userBusiness.getUserByEmail(loginData)
+            res.status(200).send({
+                message: "User logged in",
+                token
+            })
+        }
+        catch(error){
+            res.status(error.statusCode).send({
                 error:error.message
             })
         }
